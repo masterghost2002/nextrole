80f20046e72faae96cd51ed68fc0f2f7b014208c
Rakesh Dhariwal <rakeshdhariwal61@gmail.com>
2025-02-15 17:58:04 +0530

    feat: improvement

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,30 +1,39 @@
 import { NextRequest, NextResponse } from "next/server";
 // The client you created from the Server-Side Auth instructions
 import { createClient } from "@/lib/supabase";
+import type { Database } from "@/types/supabase";
+
 export async function GET(request: NextRequest) {
   const { searchParams, origin } = new URL(request.url);
   const code = searchParams.get("code");
   // if "next" is in param, use it as the redirect URL
   const next = searchParams.get("next") ?? "/";
 
-  if (code) {
-    const supabase = await createClient();
-    const { error, data } = await supabase.auth.exchangeCodeForSession(code);
-    console.log(data);
-    if (!error) {
-      const forwardedHost = request.headers.get("x-forwarded-host"); // original origin before load balancer
-      const isLocalEnv = process.env.NODE_ENV === "development";
-      if (isLocalEnv) {
-        // we can be sure that there is no load balancer in between, so no need to watch for X-Forwarded-Host
-        return NextResponse.redirect(`${origin}${next}`);
-      } else if (forwardedHost) {
-        return NextResponse.redirect(`https://${forwardedHost}${next}`);
-      } else {
-        return NextResponse.redirect(`${origin}${next}`);
-      }
-    }
+  if (!code) {
+    return NextResponse.redirect(`${origin}/404`);
   }
 
-  // return the user to an error page with instructions
-  return NextResponse.redirect(`${origin}/auth/auth-code-error`);
+  const supabase = await createClient();
+  const { error, data } = await supabase.auth.exchangeCodeForSession(code);
+
+  if (error) {
+    return NextResponse.redirect(`${origin}/404`);
+  }
+
+  const userFromDatabase = await supabase
+    .from("users")
+    .select("*")
+    .eq("email", data.user.email)
+    .single();
+  if (!userFromDatabase.data) return NextResponse.redirect(`${origin}/onboard`);
+
+  const forwardedHost = request.headers.get("x-forwarded-host"); // original origin before load balancer
+  const isLocalEnv = process.env.NODE_ENV === "development";
+  if (isLocalEnv) {
+    // we can be sure that there is no load balancer in between, so no need to watch for X-Forwarded-Host
+    return NextResponse.redirect(`${origin}${next}`);
+  } else if (forwardedHost) {
+    return NextResponse.redirect(`https://${forwardedHost}${next}`);
+  }
+  return NextResponse.redirect(`${origin}${next}`);
 }
