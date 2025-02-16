f4e480e8cb5dd7502300af1a143a4e629c2f2391
Rakesh Dhariwal <rakeshdhariwal61@gmail.com>
2025-02-16 14:05:50 +0530

    add:avatar class in db

--- lib/supabase/db/index.ts
+++ lib/supabase/db/index.ts
@@ -2,14 +2,17 @@ import { createClient } from "@/lib/supabase";
 import type { Database } from "@/types/supabase";
 import { SupabaseClient } from "@supabase/supabase-js";
 import { User } from "./user"; // Assuming User class is in a user.ts file in the same directory
+import { Avatar } from "./avatar";
 
 export default class DB {
   private static instance: DB;
-  public user: User;
+  private user: User;
+  private avatar: Avatar;
 
   private constructor(protected supabase: SupabaseClient<Database>) {
     this.supabase = supabase;
     this.user = new User(this.supabase);
+    this.avatar = new Avatar(this.supabase);
   }
 
   public static async getInstance(): Promise<DB> {
