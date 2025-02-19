a873154aa5e149de15ceafe6b63a3249fae3934d
Rakesh Dhariwal <rakeshdhariwal61@gmail.com>
2025-02-19 16:13:29 +0530

    feat:db-injector

--- core/controller/avatar.ts
+++ core/controller/avatar.ts
@@ -1,10 +1,12 @@
 import DB from "@/core/db";
-import { basicAuth } from "../decorators";
+import { InjectDB, basicAuth } from "@/core/decorators";
+@InjectDB()
 export class AvatarController {
+  private db!: Promise<DB>;
   constructor() {}
   @basicAuth()
   async getAllAvatars() {
-    const db = await DB.getInstance();
+    const db = await this.db;
     const avatars = await db.avatar.getAllAvatars();
     return avatars;
   }
---
+++ core/decorators/db.ts
@@ -0,0 +1,11 @@
+import DB from "@/core/db";
+export function InjectDB() {
+  return function <T extends { new (...args: any[]): {} }>(constructor: T) {
+    return class extends constructor {
+      constructor(...args: any[]) {
+        super(...args);
+        (this as any).db = DB.getInstance();
+      }
+    };
+  };
+}
