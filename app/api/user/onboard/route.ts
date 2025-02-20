b3223448606f67d011a76fa876e5e0475698dd9c
Rakesh Dhariwal <rakeshdhariwal61@gmail.com>
2025-02-20 21:21:01 +0530

    fix: getCookie called outside

--- app/api/user/onboard/route.ts
+++ app/api/user/onboard/route.ts
@@ -1,8 +1,8 @@
 import { withErrorHandling } from "@/core";
 import { NextResponse } from "next/server";
 import { UserController } from "@/core/controller";
-const userController = new UserController();
 export const POST = withErrorHandling(async (request: Request) => {
+  const userController = new UserController();
   const body = await request.json();
   const response = await userController.createUser(body);
   return NextResponse.json(response);
