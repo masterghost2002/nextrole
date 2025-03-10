ac62ddcb090479e5f72963b39ff566c179e634be
Rakesh Dhariwal <rakeshdhariwal61@gmail.com>
2025-03-10 13:59:33 +0530

    fix:domain

--- app/(ui)/(protected)/company/[id]/page.tsx
+++ app/(ui)/(protected)/company/[id]/page.tsx
@@ -1,10 +1,11 @@
+const DOMAIN = process.env.NEXT_PUBLIC_DOMAIN;
 export default async function Page({
   params,
 }: {
   params: Promise<{ id: string }>;
 }) {
   const { id } = await params;
-  const respnse = await fetch(`http://localhost:3000/api/company?id=${id}`);
+  const respnse = await fetch(`${DOMAIN}/api/company?id=${id}`);
   const data = await respnse.json();
   return (
     <div className="text-neutral-900">My Post: {JSON.stringify(data)}</div>
