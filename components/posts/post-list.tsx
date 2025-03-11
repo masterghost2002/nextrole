c52557a19a0753b71c1bbe1f7a1e7d6db19900e9
Rakesh Dhariwal <rakeshdhariwal61@gmail.com>
2025-03-11 14:35:50 +0530

    replace spinner for skeleton for loading

--- components/posts/post-list.tsx
+++ components/posts/post-list.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useRef } from 'react';
 import { InfiniteData } from '@tanstack/react-query';
 import { PostCard } from './post-card';
+import { PostCardSkeleton } from '../skeletons';
 
 type TProps = {
   data: InfiniteData<TPost[]>;
@@ -53,9 +54,13 @@ export const PostList = ({
 
   if (isLoading)
     return (
-      <div className="flex justify-center py-10">
-        <div className="h-8 w-8 animate-spin rounded-full border-4 border-gray-300 border-t-blue-500"></div>
-      </div>
+      <>
+        <PostCardSkeleton />
+        <PostCardSkeleton />
+        <PostCardSkeleton />
+        <PostCardSkeleton />
+        <PostCardSkeleton />
+      </>
     );
 
   return (
