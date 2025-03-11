b78ffdef007f231e67b28a71e405e4749d36c9b9
Rakesh Dhariwal <rakeshdhariwal61@gmail.com>
2025-03-11 09:25:43 +0530

    redirection fix

--- actions/company.ts
+++ actions/company.ts
@@ -2,6 +2,7 @@ const DOMAIN = process.env.NEXT_PUBLIC_DOMAIN;
 export const getCompanyById = async (id: string) => {
   try {
     const response = await fetch(`${DOMAIN}/api/company?id=${id}`);
+    if (!response.ok) throw response;
     const companyData = await response.json();
     return companyData as TCompany;
   } catch (error) {
